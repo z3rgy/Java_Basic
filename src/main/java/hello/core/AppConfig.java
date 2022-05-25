d5b4a5ac37e4923f5db2b1bf3451045382c12e2f
z3rgy <loveholic.g@gmail.com>
2022-05-25 11:21:08 +0900

    discountPolicy fix->rate 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -18,11 +19,14 @@ public class AppConfig {
     private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+        //return new FixDiscountPolicy();
+
     }
 }
